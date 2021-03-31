11d3e25ad47c7de411afe45a562f4543a689b25c
Alya Bouguila <alya.bouguila@esprit.tn>
2021-03-31 14:06:36 +0100

    my commit

---
+++ src/Entity/CentreMedical.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CentreMedicalRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CentreMedicalRepository::class)
+ */
+class CentreMedical
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_centre_medical;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $numTel;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCentreMedical(): ?string
+    {
+        return $this->nom_centre_medical;
+    }
+
+    public function setNomCentreMedical(string $nom_centre_medical): self
+    {
+        $this->nom_centre_medical = $nom_centre_medical;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getNumTel(): ?int
+    {
+        return $this->numTel;
+    }
+
+    public function setNumTel(?int $numTel): self
+    {
+        $this->numTel = $numTel;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Service.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ServiceRepository::class)
+ */
+class Service
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_centre_medical;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_patient;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $service;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $matricule;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCentreMedical(): ?string
+    {
+        return $this->nom_centre_medical;
+    }
+
+    public function setNomCentreMedical(string $nom_centre_medical): self
+    {
+        $this->nom_centre_medical = $nom_centre_medical;
+
+        return $this;
+    }
+
+    public function getNomPatient(): ?string
+    {
+        return $this->nom_patient;
+    }
+
+    public function setNomPatient(string $nom_patient): self
+    {
+        $this->nom_patient = $nom_patient;
+
+        return $this;
+    }
+
+    public function getService(): ?string
+    {
+        return $this->service;
+    }
+
+    public function setService(string $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    public function getMatricule(): ?string
+    {
+        return $this->matricule;
+    }
+
+    public function setMatricule(string $matricule): self
+    {
+        $this->matricule = $matricule;
+
+        return $this;
+    }
+}
